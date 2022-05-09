eb6a670d461e71d302210a19b80865ca541084e1
soiyo <soiyosauce@gmail.com>
2022-05-09 11:46:19 +0900

    Add: static files(js, css, image)

---
+++ instagram/static/mainpage.js
@@ -0,0 +1,22 @@
+//모달
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const btnOpenPopup = document.querySelector('.btn-open-popup');
+
+btnOpenPopup.addEventListener('click', () => {
+    modal.classList.toggle('show');
+
+    if (modal.classList.contains('show')) {
+        body.style.overflow = 'hidden';
+    }
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.toggle('show');
+
+        if (!modal.classList.contains('show')) {
+            body.style.overflow = 'auto';
+        }
+    }
+});
